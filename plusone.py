da2bec533d938e650a347a474feb8d35df12e38a
PARK JOON GYU <nattybear@naver.com>
2019-01-11 22:53:39 +0900

    Add List Comprehension

--- plusone.py
+++ plusone.py
@@ -3,6 +3,10 @@ def plus(l):
     for i in l :
         result.append(i + 1)
     return(result)
+
+# List Comprehension
+def add_one(xs):
+    return [x + 1 for x in xs]
     
 #input
 num = input().split(' ')
